26c804e4e1016f1ff88fa7cae6bde7910ad79d74
Arytwsrjr <Thawornsirijara_a@su.ac.th>
2024-12-21 15:32:30 +0700

    testchange

--- demoproject/lib/main.dart
+++ demoproject/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'homepage.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -13,11 +11,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
     title: 'Flutter Demo',
+    debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
-    );
+      home: Scaffold(
+        appBar: AppBar(title: const Text ('Flutter Demo',
+          style :TextStyle(color: Colors.yellow),
+          ),
+          leading: const Icon(Icons.add),
+          backgroundColor: const Color.fromARGB(255, 235, 148, 121),
+          ),
+        
+        body:Container(
+          color: const Color.fromARGB(255, 226, 168, 141),
+          padding: const EdgeInsets.all(20),//padding ใช้ EdgeInsets
+          margin: const EdgeInsets.all(20),
+          child:const Text('hello'),
+        ),
+        
+        floatingActionButton: FloatingActionButton(onPressed: (){
+          print("Hello");
+        }
+        ,
+        
+        child: const Icon(Icons.phone),
+        
+        ),
+    ));
   }
 }
